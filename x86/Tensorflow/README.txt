722fc944982471eef9d72056549286229c044ecd
AdamSimpson <adambsimpson@gmail.com>
2017-05-08 14:00:06 -0400

    Fix incorrect symlink creation

--- x86/Tensorflow/README.txt
+++ x86/Tensorflow/README.txt
@@ -4,4 +4,7 @@ Provides Tensorflow/1.0.1 with Python2/3 interface. Additional Keras support pro
 sudo singularity create --force --size 30000 tensorflow_x86.img
 HOME=`pwd` sudo -E singularity bootstrap tensorflow_x86.img tensorflow.def
 
+# Create symlinks to Titan specific libraries and binaries
+sudo singularity exec -w tensorflow_x86.img ./symlinks.sh
+
 #aprun -n 1 singularity exec -H `pwd`:/ccs/home/atj -W `pwd` ompi_base.img ./launch.sh
--- x86/Tensorflow/symlinks.sh
+++ x86/Tensorflow/symlinks.sh
@@ -3,6 +3,11 @@
 ### Create symlinks between /opt/cray/nvidia/lib64/* and /usr/lib
 # sudo singularity exec -w ./symlinks.sh
 
+# Expand full path, following symlink which isn't visible in container
+# note that root doesn't correctly see /etc/alternatives on Titan so we hardcode the current path
+#nvidia_path=`readlink -f /opt/cray/nvidia/default`
+nvidia_path=/opt/cray/nvidia/352.101-1_1.0502.2465.0.0.gem
+
 libs=(
     'libEGL.so'
     'libcuda.so'
@@ -29,24 +34,24 @@ libs=(
 )
 
 for lib in ${libs[@]}; do
-  ln -nsf /opt/cray/nvidia/lib64/${lib}.352.101 /usr/lib/${lib}.352.101
+  ln -nsf ${nvidia_path}/lib64/${lib}.352.101 /usr/lib/${lib}.352.101
   ln -nsf /usr/lib/${lib}.352.101 /usr/lib/${lib}.1
   ln -nsf /usr/lib/${lib}.1 /usr/lib/${lib}
   
 done
 
-ln -nsf /opt/cray/nvidia/lib64/libOpenCL.so.1.0.0 /usr/lib/libOpenCL.so.1.0.0
+ln -nsf ${nvidia_path}/lib64/libOpenCL.so.1.0.0 /usr/lib/libOpenCL.so.1.0.0
 ln -nsf /usr/lib/libOpenCL.so.1.0.0 /usr/lib/libOpenCL.so.1
 ln -nsf /usr/lib/libOpenCL.so.1 /usr/lib/libOpenCL.so 
 
-ln -nsf /opt/cray/nvidia/lib64/libGLESv2.so.352.101 /usr/lib/libGLESv2.so.352.101
+ln -nsf ${nvidia_path}/lib64/libGLESv2.so.352.101 /usr/lib/libGLESv2.so.352.101
 ln -nsf /usr/lib/libGLESv2.so.352.101 /usr/lib/libGLESv2.so.2
 ln -nsf /usr/lib/libGLESv2.so.2 /usr/lib/libGLESv2.so
 
-ln -nsf /opt/cray/nvidia/lib64/nvidia_drv.so /usr/lib/nvidia_drv.so
-ln -nsf /opt/cray/nvidia/lib64/tls_test_dso.so /usr/lib/tls_test_dso.so
+ln -nsf ${nvidia_path}/lib64/nvidia_drv.so /usr/lib/nvidia_drv.so
+ln -nsf ${nvidia_path}/tls_test_dso.so /usr/lib/tls_test_dso.so
 mkdir -p /usr/lib/tls
-ln -nsf /opt/cray/nvidia/lib64/tls/libnvidia-tls.so.352.101 /usr/lib/tls/libnvidia-tls.so.352.101
+ln -nsf ${nvidia_path}/tls/libnvidia-tls.so.352.101 /usr/lib/tls/libnvidia-tls.so.352.101
 
 ### Create symlinks between libmpi* and Cray's mpich
 mv /usr/lib/x86_64-linux-gnu/libmpich.so.12.1.0 /usr/lib/x86_64-linux-gnu/libmpich.so.12.1.0.original
@@ -58,5 +63,5 @@ ln -nsf /opt/cray/mpt/7.5.2/gni/mpich-gnu/5.1/lib/libmpichcxx.so /usr/lib/x86_64
 ln -nsf /opt/cray/mpt/7.5.2/gni/mpich-gnu/5.1/lib/libfmpich.so /usr/lib/x86_64-linux-gnu/libmpichfort.so.12.1.0
 
 ### Create symlinks between /opt/cray/nvidia/bin/* and /usr/bin
-ln -nsf /opt/cray/nvidia/bin/nvidia-debugdump  /usr/bin/nvidia-debugdump
-ln -nsf /opt/cray/nvidia/bin/nvidia-smi  /usr/bin/nvidia-smi
+ln -nsf ${nvidia_path}/bin/nvidia-debugdump  /usr/bin/nvidia-debugdump
+ln -nsf ${nvidia_path}/bin/nvidia-smi  /usr/bin/nvidia-smi
